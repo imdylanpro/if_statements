4d0ce058bc1bf9c9e123b059a3fbca3401de62aa
imdylanpro <159092190+imdylanpro@users.noreply.github.com>
2024-02-09 12:28:25 -0800

    Update ififif.py
    Commented out the original code. Added a for loop that does the same thing as the original code, but in 3 lines rather than 14.

--- ififif.py
+++ ififif.py
@@ -10,17 +10,21 @@ requested_toppings = ['pepperoni', 'sausage', 'anchovies', 'extra cheese',
                        'olives', 'chicken']
 
 # Prints a message for each topping that is included in requested_toppings list
-if 'pepperoni' in requested_toppings:
-    print('Pepperoni will be added to your pizza!')
-if 'sausage' in requested_toppings:
-    print('Sausage will be added to your pizza!')
-if 'anchovies' in requested_toppings:
-    print('Anchovies will be added to your pizza!')
-if 'extra cheese' in requested_toppings:
-    print('Extra cheese will be added to your pizza!')
-if 'olives' in requested_toppings:
-    print('Olives will be added to your pizza!')
-if 'chicken' in requested_toppings:
-    print('Chicken will be added to your pizza!')
-if 'pickles' in requested_toppings:
-    print('Pickles will be added to your pizza!')
+# if 'pepperoni' in requested_toppings:
+#     print('Pepperoni will be added to your pizza!')
+# if 'sausage' in requested_toppings:
+#     print('Sausage will be added to your pizza!')
+# if 'anchovies' in requested_toppings:
+#     print('Anchovies will be added to your pizza!')
+# if 'extra cheese' in requested_toppings:
+#     print('Extra cheese will be added to your pizza!')
+# if 'olives' in requested_toppings:
+#     print('Olives will be added to your pizza!')s
+# if 'chicken' in requested_toppings:
+#     print('Chicken will be added to your pizza!')
+# if 'pickles' in requested_toppings:
+#     print('Pickles will be added to your pizza!')
+
+for requested_topping in requested_toppings:
+    if requested_topping in requested_toppings:
+        print(f'{requested_topping.title()} will be added to your pizza!')
